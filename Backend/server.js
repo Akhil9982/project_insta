e79c56cedcc71b026954b7afee1a0cc9a870d2ee
Akhil Battula <b_akhilkumar@outlook.com>
2026-05-28 14:40:22 +0530

    Updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
+
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
-connectDB();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed", err);
+  });
